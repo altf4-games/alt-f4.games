82a476e5cc740640e44e86b4682f8325e92558c6
FreezeStudios <39883432+FreezeStudios@users.noreply.github.com>
2023-08-07 21:51:42 +0530

    Added Web Editor Project
    Added Web Editor Project

---
+++ web-editor/index.js
@@ -0,0 +1,81 @@
+function run()
+{
+    let html = document.getElementById("html").value;
+    let css = document.getElementById("css").value;
+    let js = document.getElementById("js").value;
+    let frameObj = document.getElementById("output");
+
+    frameObj.contentWindow.document.body.innerHTML =  "<style>" + css + "</style>" + html;
+    frameObj.contentWindow.eval(js);
+    try {
+      const result = eval(js);
+      if (result !== undefined) {
+        console.log(result);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+const downloadFile = () => {
+    const link = document.createElement("a");
+    const content = "<html><head><style>" + document.getElementById("css").value + "</style>"  + "<script>" + document.getElementById("js").value  + "</script></head><body>" + document.getElementById("html").value + "</body></html>"; 
+    const file = new Blob([content], { type: 'text/plain' });
+    link.href = URL.createObjectURL(file);
+    link.download = "index.html";
+    link.click();
+    URL.revokeObjectURL(link.href);
+ };
+
+ async function generateText() {
+    const promptText = document.getElementById("input-field").value;
+    const prompt_template = `<|system|>\n<|end|>\n<|user|>\n${promptText}<|end|>\n<|assistant|>`;
+  
+    try {
+      const response = await axios.post(apiEndpoint, {
+        inputs: promptText,
+        options: {
+          max_new_token:512,
+          temperature:0.2,
+          do_sample:true,
+          top_k:50,
+          top_p:0.95,
+          return_full_text: true,
+        },
+      }, {
+        headers: {
+          'Authorization': `Bearer ${apiToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      const generatedText = response.data[0]?.generated_text;
+      document.getElementById("input-field").value = generatedText;
+    } catch (error) {
+      console.error('Error generating text:', error);
+    }
+}
+
+console = {
+  log: function(message) {
+    logToConsole(message, 'log');
+  },
+  error: function(message) {
+    logToConsole(message, 'error');
+  },
+  warn: function(message) {
+    logToConsole(message, 'warn');
+  }
+};
+
+function logToConsole(message, type = 'log') {
+  const consoleElement = document.getElementById('console');
+  const logItem = document.createElement('div');
+  logItem.classList.add(type);
+  logItem.textContent = message;
+  consoleElement.appendChild(logItem);
+  consoleElement.scrollTop = consoleElement.scrollHeight;
+}
+
+const apiToken = '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'; //pls don't abuse this token :)
+const apiEndpoint = "https://api-inference.huggingface.co/models/HuggingFaceH4/starchat-beta";

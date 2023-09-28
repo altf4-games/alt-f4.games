c6226b5cd46697bfd33809df58f8004d3aab9724
FreezeStudios <39883432+AltF4-Games@users.noreply.github.com>
2023-09-28 10:09:32 +0530

    Google Analytics & Prettier

--- index.js
+++ index.js
@@ -2,38 +2,38 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function changeSlide(n) {
-    showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 setInterval(() => {
-    showSlides(++slideIndex)
-},10000);
+  showSlides(++slideIndex);
+}, 10000);
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("img");
-    let dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("img");
+  let dots = document.getElementsByClassName("dot");
 
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
 
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
 
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }

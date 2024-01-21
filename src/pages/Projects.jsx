c8822db1e908cb73fd06c37ae1fa14ad54ed2844
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2024-01-21 19:41:51 +0530

    Updated games.json & Fixed Minor Bugs

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -20,7 +20,7 @@ const Projects = () => {
 
   return (
     <div className="container mt-4">
-      <h2 className="mb-4">GitHub Repositories</h2>
+      <h2 className="mb-4">GITHUB REPOSITORIES</h2>
       <div className="row">
         {repositories.map((repo) => (
             <ProjectCard repo={repo} key={repo.id} />

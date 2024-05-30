a51f48be1cd3982a281141b0185b33a9bbf542f9
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2024-05-30 19:09:37 +0530

    Fixed DarkReader CSS

--- src/components/ThreeScene.jsx
+++ src/components/ThreeScene.jsx
@@ -9,7 +9,7 @@ const ThreeScene = () => {
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
         renderer.setSize(window.innerWidth, window.innerHeight);
-        scene.background = new THREE.Color(0x181818);
+        scene.background = new THREE.Color(0x121414);
 
         // Lighting
         const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
